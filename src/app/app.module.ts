e6f9552f61a9cc37f12484dddd918bfb33701f28
Kumar <mohitnevatru@gmail.com>
2021-10-23 14:29:54 +0530

    clean up and all components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LogFormComponent } from './components/log-form/log-form.component';
+import { LogComponent } from './components/log/log.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LogFormComponent,
+    LogComponent
   ],
   imports: [
     BrowserModule
