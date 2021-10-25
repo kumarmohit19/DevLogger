7d92c1b992c1d08b3d4ce6c5454047516926e0a7
Kumar <mohitnevatru@gmail.com>
2021-10-25 13:18:51 +0530

    clear button issue fixed

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -21,23 +21,24 @@ export class LogService {
   stateClear = this.stateSource.asObservable();
 
   constructor() {
-    this.logs = [
-      // {
-      //   id: '1',
-      //   text: 'Generated Component',
-      //   date: new Date('12/26/2018 12:54:23'),
-      // },
-      // {
-      //   id: '2',
-      //   text: 'Added Bootstrap',
-      //   date: new Date('12/27/2018 02:40:23'),
-      // },
-      // {
-      //   id: '3',
-      //   text: 'Added logs Component',
-      //   date: new Date('12/28/2018 12:54:23'),
-      // },
-    ];
+    // this.logs = [
+    //   {
+    //     id: '1',
+    //     text: 'Generated Component',
+    //     date: new Date('12/26/2018 12:54:23'),
+    //   },
+    //   {
+    //     id: '2',
+    //     text: 'Added Bootstrap',
+    //     date: new Date('12/27/2018 02:40:23'),
+    //   },
+    //   {
+    //     id: '3',
+    //     text: 'Added logs Component',
+    //     date: new Date('12/28/2018 12:54:23'),
+    //   },
+    // ];
+    this.logs = [];
   }
 
   getLogs(): Observable<Log[]> {
